9c243ac9b07d52c8eab5f238e9d6fb2015a3fd8a
dreplica <adejo.david@aiesec.net>
2020-02-17 14:46:27 +0100

    implemented a search component

--- src/Components/contacts/contactListing.tsx
+++ src/Components/contacts/contactListing.tsx
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { userState, dataType } from '../../Redux/Reducer/userReducer';
+import React, { useEffect} from 'react';
+import { userState} from '../../Redux/Reducer/userReducer';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Search from './searchBar';
 
 interface Iprops{
   data: string | number | Array<object>
+  obj?:{[key:string]:string}
 }
 
 const ShowContacts: React.FC<Iprops> = ({ data }) => {
-  const [Contacts , setContacts] = useState(data)
-  console.log(data)
+  const history = useHistory()
+  const data_asArray = data as Array<Iprops['obj']>
   useEffect(() => {
-    if (data) { 
-      setContacts(data)
-     }
+    
    }, [data])
-  console.log(typeof Contacts)
-    // Contacts.map((value:object,index:string)=><li key={index} id={value.id}>value</li>)
-
+  const handleClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>)=>{
+    e.preventDefault();
+    const id = e.currentTarget.id
+    history.push(`/contact/${id}`)
+   }
   return (
     <>
-    
+      <ul>
+        {data_asArray.map((value: Iprops['obj'], index: number) => <li key={index} id={value?.id} onClick={handleClick}>{value?.fname}</li>)}
+      </ul>
+      <Search />
     </>
   );
 }
--- src/Components/contacts/searchBar.tsx
+++ src/Components/contacts/searchBar.tsx
@@ -1,1 +1,37 @@
-export default {}
+import React, { useState, ChangeEvent } from 'react';
+import { dataType, userState } from '../../Redux/Reducer/userReducer';
+import { connect } from 'react-redux';
+
+interface Iprops{
+    data:dataType
+}
+
+const Search: React.FC<Iprops> = ({ data }) => {
+    const [searchdata, setsearchdata] = useState<Array<{ [key: string]: string }>>()
+    
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        console.log(e.target?.value.length)
+        const reg = new RegExp(`${(e.target?.value)}`, 'ig')
+        const Contact = data.Profile[0].Contacts as Array<{ [key: string]: string }>
+        const filter = Contact.filter((x,i) =>x.fname.match(reg))
+        setsearchdata(filter)
+        if ((e.target?.value).length === 0) {
+                setsearchdata([])
+                }
+    }
+  return (
+      <>
+          <label>Search
+            <input type='search' name='search' onChange={handleChange} />
+              {searchdata?.map((x,i) => <li key={i}>{x?.fname}</li>)}
+          </label>
+    </>
+  );
+}
+
+const mapStateToProps = ({ userReducer }:{ userReducer: userState }) => ({
+    data:userReducer.data
+})
+
+export default connect(mapStateToProps)(Search)
